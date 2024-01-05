2df97cca4103e62244f6cc928888bb302f74edb9
sohel1807 <114603804+sohel1807@users.noreply.github.com>
2024-01-05 21:09:24 +0530

    Update iplapp.py

--- iplapp.py
+++ iplapp.py
@@ -42,7 +42,10 @@ if st.button('Predict probability'):
     RRR=(runs_left*6)/balls_left
     final=pd.DataFrame({"BattingTeam":[BattingTeam],"BowlingTeam":[BowlingTeam],"City":[city],"runs_left":[runs_left],"total_run_y":[total_run_y],"balls_left":[balls_left],"wickets_left":[wickets_left],"CRR":[CRR],"RRR":[RRR]})
     result=pipe.predict_proba(final)
-    if(wickets_left==0):
+    if(currentscore>=total_run_y):
+        st.text(BattingTeam + ":-100%")
+        st.text(BowlingTeam + ":-0%")
+    elif(wickets_left==0):
         st.text(BattingTeam + ":-0%")
         st.text(BowlingTeam + ":-100%")
     else:
