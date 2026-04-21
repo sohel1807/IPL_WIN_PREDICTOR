e157d10fdc132dd3f6cc8421d3f1c4aaf68cdd59
sohel <sohelmujawar1807@gmail.com>
2026-04-21 22:09:48 +0530

    xyz

--- iplapp.py
+++ iplapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import sklearn
-pipe=pickle.load(open("pipe1.pkl","rb"))
+pipe=pickle.load(open("pipe.pkl","rb"))
 page_by_img='''
 <style>
 body{
